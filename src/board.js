b7e4b1f8a61ab99cebc388a1f012febcb8cc0c80
esmaeil1377 <pahang1378@gmail.com>
2021-02-12 14:33:00 +0330

    board update

---
+++ src/board.js
@@ -0,0 +1,58 @@
+"use strict";
+
+var rotateLeft = function rotateLeft(matrix) {
+  var rows = matrix.length;
+  var columns = matrix[0].length;
+  var res = [];
+  for (var row = 0; row < rows; ++row) {
+    res.push([]);
+    for (var column = 0; column < columns; ++column) {
+      res[row][column] = matrix[column][columns - row - 1];
+    }
+  }
+  return res;
+};
+
+var Tile = function Tile(value, row, column) {
+  this.value = value || 0;
+  this.row = row || -1;
+  this.column = column || -1;
+  this.oldRow = -1;
+  this.oldColumn = -1;
+  this.markForDeletion = false;
+  this.mergedInto = null;
+  this.id = Tile.id++;
+};
+
+Tile.id = 0;
+
+Tile.prototype.moveTo = function (row, column) {
+  this.oldRow = this.row;
+  this.oldColumn = this.column;
+  this.row = row;
+  this.column = column;
+};
+
+Tile.prototype.isNew = function () {
+  return this.oldRow == -1 && !this.mergedInto;
+};
+
+Tile.prototype.hasMoved = function () {
+  return this.fromRow() != -1 && (this.fromRow() != this.toRow() || this.fromColumn() != this.toColumn()) || this.mergedInto;
+};
+
+Tile.prototype.fromRow = function () {
+  return this.mergedInto ? this.row : this.oldRow;
+};
+
+Tile.prototype.fromColumn = function () {
+  return this.mergedInto ? this.column : this.oldColumn;
+};
+
+Tile.prototype.toRow = function () {
+  return this.mergedInto ? this.mergedInto.row : this.row;
+};
+
+Tile.prototype.toColumn = function () {
+  return this.mergedInto ? this.mergedInto.column : this.column;
+};
