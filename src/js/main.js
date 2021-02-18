5150c946e8e049c8ef4716cf639f94493b3405b7
esmaeil1377 <pahang1378@gmail.com>
2021-02-18 09:22:46 +0330

    update for login and signup

--- src/js/main.js
+++ src/js/main.js
@@ -64,3 +64,56 @@ function redirect(){
     app.get('/', (req, res) => res.send('Hello World!'))
     app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 }
+
+const URL = "http://localhost:3000" 
+// https://tonyspiro.com/how-to-keep-session-data-after-fetch-post-with-express/
+Parse.initialize("myAppId2", "myMasterKey2"); //PASTE HERE YOUR Back4App APPLICATION ID AND YOUR JavaScript KEY
+Parse.serverURL = "http://localhost:1337/parse";
+window.onload = function () {
+    document.getElementById("signin1").onclick = function() {
+        //let username = document.getElementById("usernamee").value
+        //let password = "thisispassword"
+        let password = document.getElementById("password1").value
+        //let email = `${username}@gmail.com`
+        let email = document.getElementById("email1").value
+        const data = { email, password};
+
+        fetch(URL + "/api/signin", {
+            method: 'POST',
+            //credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+        
+    }
+
+    document.getElementById("signupp").onclick = function() {
+        gethttp(URL + "/api/getcurrentuser")
+        .then(res => {
+            debugger
+            let user = JSON.parse(res.body).message
+
+            console.log(user)
+            document.getElementById("user").innerHTML = user.username
+            
+        }, err => {
+            console.log(err)
+        })
+    }
+}
+
+async function gethttp(url) {
+    let response = await fetch(url);
+    let responsemessage = await response.text();
+    return { body: responsemessage, status: response.status };
+}
+
